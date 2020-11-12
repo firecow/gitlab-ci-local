85abd77e5bb6287ae63be382abb4092eed4ba8ea
Mads Jon Nielsen <madsjon@gmail.com>
2020-11-12 11:49:15 +0100

    Bump version
    Former-commit-id: 6f61f39fc2ff08b84235dc61fceaf88a060bdcb5

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ Array.prototype.first = function() {
 };
 
 const argv = yargs
-    .version("4.5.0")
+    .version("4.5.1")
     .showHelpOnFail(false)
     .wrap(180)
     .command(defaultCmd as CommandModule)
