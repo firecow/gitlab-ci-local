7080d74eadb158ae3a3a0762e4f9bc34844d5fe7
Mads Jon Nielsen <madsjon@gmail.com>
2021-01-17 09:07:57 +0100

    Added more tests
    Former-commit-id: d418a3ff5b907b1518b4212f08bb2ffb3d401a69

--- src/tests/print-to-stream.test.ts
+++ src/tests/print-to-stream.test.ts
@@ -14,3 +14,17 @@ test('Print to stderr', () => {
     expect(mockStderr).toHaveBeenCalledWith('[31mHello, world![39m\n');
     mockStderr.mockRestore();
 });
+
+test('Print job on index 0', () => {
+    const mockStdout = mockProcess.mockProcessStdout();
+    Utils.printJobNames({name: "Hello"}, 0, [{name: "Hello"}, {name: "Hello"}, {name: "Hello"}]);
+    expect(mockStdout).toHaveBeenCalledWith('[94mHello[39m, ');
+    mockStdout.mockRestore();
+});
+
+test('Print job on last index', () => {
+    const mockStdout = mockProcess.mockProcessStdout();
+    Utils.printJobNames({name: "Hello"}, 2, [{name: "Hello"}, {name: "Hello"}, {name: "Hello"}]);
+    expect(mockStdout).toHaveBeenCalledWith('[94mHello[39m');
+    mockStdout.mockRestore();
+});
