a9166db11f689ea1087f212da7de0ae3c42c9253
Mads Jon Nielsen <madsjon@gmail.com>
2021-01-19 15:22:40 +0100

    Simplify bash completion in README

--- README.md
+++ README.md
@@ -79,29 +79,8 @@ echo "alias gcl='gitlab-ci-local'" >> ~/.bashrc
 ```
 
 ### Bash completion
-
-Add this to `~/.bashrc`
 ```bash
-_yargs_completions()
-{
-    local cur_word args type_list
-
-    cur_word="${COMP_WORDS[COMP_CWORD]}"
-    args=("${COMP_WORDS[@]}")
-
-    # ask yargs to generate completions.
-    type_list=$(/usr/local/bin/gitlab-ci-local --get-yargs-completions "${args[@]}")
-
-    COMPREPLY=( $(compgen -W "${type_list}" -- ${cur_word}) )
-
-    # if no match was found, fall back to filename completion
-    if [ ${#COMPREPLY[@]} -eq 0 ]; then
-      COMPREPLY=()
-    fi
-
-    return 0
-}
-complete -o default -F _yargs_completions gitlab-ci-local
+gitlab-ci-local --completion >> ~/.bashrc 
 ```
 
 ## Quirks
--- src/default_cmd.ts
+++ src/default_cmd.ts
@@ -5,6 +5,18 @@ import {Commander} from "./commander";
 import {Parser} from "./parser";
 import * as state from "./state";
 
+const checkFolderAndFile = (cwd: string) => {
+    if (!fs.pathExistsSync(`${cwd}`)) {
+        process.stdout.write(`${cwd} is not a directory\n`);
+        process.exit(1);
+    }
+
+    if (!fs.existsSync(`${cwd}/.gitlab-ci.yml`)) {
+        process.stdout.write(`${cwd} does not contain .gitlab-ci.yml\n`);
+        process.exit(1);
+    }
+}
+
 exports.command = "$0 [job]";
 exports.describe = "Runs the entire pipeline or a single [job]";
 exports.builder = (y: any) => {
@@ -16,16 +28,6 @@ exports.builder = (y: any) => {
 exports.handler = async(argv: any) => {
     const cwd = argv.cwd ? path.resolve(argv.cwd) : process.cwd();
 
-    if (!fs.pathExistsSync(`${cwd}`)) {
-        process.stdout.write(`${cwd} is not a directory\n`);
-        process.exit(1);
-    }
-
-    if (!fs.existsSync(`${cwd}/.gitlab-ci.yml`)) {
-        process.stdout.write(`${cwd} does not contain .gitlab-ci.yml\n`);
-        process.exit(1);
-    }
-
     if (argv.completion !== undefined) {
         yargs.showCompletionScript();
     } else if (argv.list !== undefined) {
@@ -33,10 +35,12 @@ exports.handler = async(argv: any) => {
         const parser = await Parser.create(cwd, pipelineIid);
         await Commander.runList(parser);
     } else if (argv.job) {
+        checkFolderAndFile(cwd);
         const pipelineIid = await state.getPipelineIid(cwd);
         const parser = await Parser.create(cwd, pipelineIid);
         await Commander.runSingleJob(parser, argv.job as string, argv.needs as boolean);
     } else {
+        checkFolderAndFile(cwd);
         await state.incrementPipelineIid(cwd);
         const pipelineIid = await state.getPipelineIid(cwd);
         const parser = await Parser.create(cwd, pipelineIid);
