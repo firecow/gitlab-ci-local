72abe05537cd8d540664ad6ed0231e29349f34c0
Mads Jon Nielsen <madsjon@gmail.com>
2021-01-22 20:22:54 +0100

    Add an "integration" test, and move jest config into package.json add coverage.json
    Former-commit-id: e7160babf364d13d1416318500469a452b454cbe

---
+++ src/yargs.ts
@@ -0,0 +1,32 @@
+import * as yargs from "yargs";
+import {CommandModule} from "yargs";
+import {Parser} from "./parser";
+import * as defaultCmd from "./default_cmd";
+import * as state from "./state";
+import * as fs from "fs-extra";
+import * as path from "path";
+
+
+export async function runYargs(arg = process.argv.slice(2)) {
+    const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), "utf8"));
+
+    yargs(arg)
+        .version(packageJson['version'])
+        .showHelpOnFail(false)
+        .wrap(yargs.terminalWidth())
+        .command(defaultCmd as CommandModule)
+        .usage("Find more information at https://github.com/firecow/gitlab-ci-local")
+        .strictOptions()
+        .option("manual", {type: "array", description: "One or more manual jobs to run during a pipeline", requiresArg: true})
+        .option("list", {type: "string", description: "List jobs and job information", requiresArg: false})
+        .option("cwd", {type: "string", description: "Path to a gitlab-ci.yml", requiresArg: true})
+        .option("completion", {type: "string", description: "Generate bash completion script", requiresArg: false})
+        .option("needs", {type: "boolean", description: "Run needed jobs, when executing a single job", requiresArg: false})
+        .completion("completion", false, async (_, yargsArgv) => {
+            const cwd = yargsArgv.cwd as string || process.cwd();
+            const pipelineIid = await state.getPipelineIid(cwd);
+            const parser = await Parser.create(cwd, pipelineIid, true);
+            return parser.getJobNames();
+        })
+        .argv;
+}
