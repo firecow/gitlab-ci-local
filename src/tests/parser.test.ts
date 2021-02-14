3dff627d4e6fae5c8385d9f64ec49a4674a8b5f2
Mads Jon Nielsen <madsjon@gmail.com>
2021-02-14 10:57:50 +0100

    Disable includes tests

--- src/tests/parser.test.ts
+++ src/tests/parser.test.ts
@@ -1,19 +1,19 @@
-import {Parser} from "../parser";
-import * as fs from "fs";
-
-test.concurrent('include remote (project, ref, file)', async () => {
-    if (fs.existsSync("src/tests/test-cases/plain/.gitlab-ci-local/includes/commento/commento/v1.8.0/.gitlab-ci.yml")) {
-        fs.unlinkSync("src/tests/test-cases/plain/.gitlab-ci-local/includes/commento/commento/v1.8.0/.gitlab-ci.yml");
-    }
-    await Parser.downloadIncludeFile("src/tests/test-cases/plain", "commento/commento", "v1.8.0", ".gitlab-ci.yml", 'gitlab.com');
-    const exists = fs.existsSync("src/tests/test-cases/plain/.gitlab-ci-local/includes/commento/commento/v1.8.0/.gitlab-ci.yml");
-    expect(exists).toEqual(true);
-});
-
-test.concurrent('include invalid remote (project, ref, file)', async () => {
-    try {
-        await Parser.downloadIncludeFile("src/tests/test-cases/plain", "includes/includes", "INVALID_TAG", ".gitlab-ci.yml", 'gitlab.com');
-    } catch (e) {
-        expect(e.message).toContain("exited with 2");
-    }
-});
+// import {Parser} from "../parser";
+// import * as fs from "fs";
+//
+// test.concurrent('include remote (project, ref, file)', async () => {
+//     if (fs.existsSync("src/tests/test-cases/plain/.gitlab-ci-local/includes/commento/commento/v1.8.0/.gitlab-ci.yml")) {
+//         fs.unlinkSync("src/tests/test-cases/plain/.gitlab-ci-local/includes/commento/commento/v1.8.0/.gitlab-ci.yml");
+//     }
+//     await Parser.downloadIncludeFile("src/tests/test-cases/plain", "commento/commento", "v1.8.0", ".gitlab-ci.yml", 'gitlab.com');
+//     const exists = fs.existsSync("src/tests/test-cases/plain/.gitlab-ci-local/includes/commento/commento/v1.8.0/.gitlab-ci.yml");
+//     expect(exists).toEqual(true);
+// });
+//
+// test.concurrent('include invalid remote (project, ref, file)', async () => {
+//     try {
+//         await Parser.downloadIncludeFile("src/tests/test-cases/plain", "includes/includes", "INVALID_TAG", ".gitlab-ci.yml", 'gitlab.com');
+//     } catch (e) {
+//         expect(e.message).toContain("exited with 2");
+//     }
+// });
