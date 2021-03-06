e33435141ff0f285c4b480b866c1c1acdf2b8396
Mads Jon Nielsen <madsjon@gmail.com>
2021-03-06 21:25:53 +0100

    Add composer and index.php does something now

--- examples/docker-swarm-php/index.php
+++ examples/docker-swarm-php/index.php
@@ -1,9 +1,12 @@
-const express = require('express');
-const app = express();
-app.disable('x-powered-by');
+<?php
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-});
+require_once 'vendor/autoload.php';
 
-app.listen(3000);
+use UAParser\Parser;
+
+$ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36";
+
+$parser = Parser::create();
+$result = $parser->parse($ua);
+
+echo json_encode($result, JSON_PRETTY_PRINT);
