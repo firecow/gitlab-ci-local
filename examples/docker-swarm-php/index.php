6ee1d61c74ca40ef371ac7418a9441c0619b6cbc
Mads Jon Nielsen <madsjon@gmail.com>
2021-03-06 21:50:54 +0100

    Added more to the php example

--- examples/docker-swarm-php/index.php
+++ examples/docker-swarm-php/index.php
@@ -6,7 +6,7 @@ use UAParser\Parser;
 
 $ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36";
 
-$parser = Parser::create();
+$parser = Parser::create( );
 $result = $parser->parse($ua);
 
 echo json_encode($result, JSON_PRETTY_PRINT);
