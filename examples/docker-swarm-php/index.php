ff88f5ff496fade37f20e69e44a04c6fc0605e81
Mads Jon Nielsen <madsjon@gmail.com>
2021-03-14 13:10:24 +0100

    PHP pipeline example (#87)
    Added PHP example

---
+++ examples/docker-swarm-php/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use UAParser\Parser;
+
+$ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36";
+
+$parser = Parser::create( );
+$result = $parser->parse($ua);
+
+header("Content-Type: application/json");
+
+echo json_encode($result, JSON_PRETTY_PRINT);
