64240d888a462254418ee4b87a4a1adfeaa7f37c
Mads Jon Nielsen <madsjon@gmail.com>
2021-04-24 21:35:33 +0200

    Call it /gcl-script/ inside docker container

--- src/job.ts
+++ src/job.ts
@@ -470,7 +470,7 @@ export class Job {
         await fs.chmod(`${this.cwd}/.gitlab-ci-local/scripts/${this.name}`, "0755");
 
         if (this.imageName) {
-            await Utils.spawn(`docker cp .gitlab-ci-local/scripts/. ${this.containerId}:/builds/.gitlab-ci-local/scripts/`, this.cwd);
+            await Utils.spawn(`docker cp .gitlab-ci-local/scripts/. ${this.containerId}:/gcl-scripts/`, this.cwd);
         }
 
         const cp = childProcess.spawn(this.containerId ? `docker start --attach -i ${this.containerId}` : "bash", {
@@ -502,7 +502,7 @@ export class Job {
             cp.on("error", (err) => setTimeout(() => reject(err), 10));
 
             if (this.imageName) {
-                cp.stdin.end(`/builds/.gitlab-ci-local/scripts/${this.name}`);
+                cp.stdin.end(`/gcl-scripts/${this.name}`);
             } else {
                 cp.stdin.end(`./.gitlab-ci-local/scripts/${this.name}`);
             }
