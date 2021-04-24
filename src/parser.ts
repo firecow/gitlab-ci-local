1805693522d869010aafc8d88643343f33c49528
Mads Jon Nielsen <madsjon@gmail.com>
2021-04-24 21:58:30 +0200

    Allow space in job names (#150)
    Former-commit-id: 4d1c57e9e5107763ec87479ceebc74d3150c84fe

--- src/parser.ts
+++ src/parser.ts
@@ -162,12 +162,6 @@ export class Parser {
 
         const gitlabData: any = deepExtend({}, ...yamlDataList);
 
-        // Make sure job name's doesn't contain spaces
-        // TODO: This deviates from gitlab ci behavior
-        Utils.forEachRealJob(gitlabData, jobName => {
-            assert(!jobName.includes(" "), `Jobs cannot include spaces, yet! '${jobName}'`);
-        });
-
         // Make sure artifact paths doesn't contain globstar
         // TODO: This deviates from gitlab ci behavior
         Utils.forEachRealJob(gitlabData, (jobName, jobData) => {
--- tests/test-cases/image/integration.image.test.ts
+++ tests/test-cases/image/integration.image.test.ts
@@ -2,13 +2,13 @@ import {MockWriteStreams} from "../../../src/mock-write-streams";
 import {handler} from "../../../src/handler";
 import * as chalk from "chalk";
 
-test("image <test-job>", async () => {
+test("image <test job>", async () => {
     const writeStreams = new MockWriteStreams();
     await handler({
         cwd: "tests/test-cases/image",
-        job: "test-job",
+        job: "test job",
     }, writeStreams);
-    const expected = [chalk`{blueBright test-job                } {greenBright >} Test something`];
+    const expected = [chalk`{blueBright test job                } {greenBright >} Test something`];
     expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
 });
 
