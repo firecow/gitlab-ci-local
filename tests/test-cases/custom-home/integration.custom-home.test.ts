2599a900b19af08b195144d09c34f63be53e3559
Joyce Babu <joyce@ennexa.com>
2021-04-28 03:15:13 +0530

    add tests

--- tests/test-cases/custom-home/integration.custom-home.test.ts
+++ tests/test-cases/custom-home/integration.custom-home.test.ts
@@ -11,10 +11,26 @@ test("custom-home <test-job>", async () => {
     }, writeStreams);
 
     const expected = [
-        chalk`{blueBright test-job} {greenBright >} group-global-var-override-value`,
-        chalk`{blueBright test-job} {greenBright >} project-group-var-override-value`,
-        chalk`{blueBright test-job} {greenBright >} project-var-value`,
-        chalk`{blueBright test-job} {greenBright >} Im content of a file variable`,
+        chalk`{blueBright test-job}           {greenBright >} group-global-var-override-value`,
+        chalk`{blueBright test-job}           {greenBright >} project-group-var-override-value`,
+        chalk`{blueBright test-job}           {greenBright >} project-var-value`,
+        chalk`{blueBright test-job}           {greenBright >} Im content of a file variable`,
     ];
     expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
+});
+
+test("custom-home <test-normalize-key>", async () => {
+    const writeStreams = new MockWriteStreams();
+    await handler({
+        cwd: "tests/test-cases/custom-home",
+        job: "test-normalize-key",
+        home: "tests/test-cases/custom-home/.home-normalize-key",
+    }, writeStreams);
+
+    const expected = [
+        chalk`{yellow WARNING: Interpreting 'gitlab.com:gcl/' as 'gitlab.com/gcl/'}`,
+        chalk`{yellow WARNING: Interpreting 'gitlab.com:gcl/custom-home' as 'gitlab.com/gcl/custom-home'}`,
+        // chalk`{blueBright test-normalize-key} {greenBright >} `,
+    ];
+    expect(writeStreams.stderrLines).toEqual(expect.arrayContaining(expected));
 });
