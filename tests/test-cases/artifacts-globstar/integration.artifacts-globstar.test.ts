408175d95f394140b34be4e1706133f9aabdb838
Mads Jon Nielsen <madsjon@gmail.com>
2021-05-05 19:15:29 +0200

    Support artifacts with globstar pattern (#175)
    Former-commit-id: e6ed216e0e56da3a2c2812bf8c50a32f6d65b912

---
+++ tests/test-cases/artifacts-globstar/integration.artifacts-globstar.test.ts
@@ -0,0 +1,16 @@
+import {MockWriteStreams} from "../../../src/mock-write-streams";
+import {handler} from "../../../src/handler";
+import * as chalk from "chalk";
+
+test("artifacts-globstar", async () => {
+    const writeStreams = new MockWriteStreams();
+    await handler({
+        cwd: "tests/test-cases/artifacts-globstar",
+    }, writeStreams);
+
+    const expected = [
+        chalk`{blueBright test-job} {greenBright >} Pre something`,
+    ];
+    expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
+    expect(writeStreams.stderrLines).toEqual([]);
+});
