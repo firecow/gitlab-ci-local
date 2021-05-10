0d1ca4dc9b23a29e4dc392453185c44e12a2c0e0
Mads Jon Nielsen <madsjon@gmail.com>
2021-05-10 12:10:22 +0200

    Add a message that pops up when jobs have been silent for 10 seconds

---
+++ tests/test-cases/long-running-silent-test-job/long-running-silent-test-job.test.ts
@@ -0,0 +1,17 @@
+import {MockWriteStreams} from "../../../src/mock-write-streams";
+import {handler} from "../../../src/handler";
+import * as chalk from "chalk";
+
+jest.setTimeout(13000);
+
+test.concurrent("long-running-silent-test-job <test-job>", async () => {
+    const writeStreams = new MockWriteStreams();
+    await handler({
+        cwd: "tests/test-cases/long-running-silent-test-job",
+        job: ["test-job"],
+    }, writeStreams);
+
+    expect(writeStreams.stdoutLines[5]).toEqual(chalk`{blueBright test-job} {grey > still running...}`);
+    expect(writeStreams.stdoutLines[7]).toEqual(chalk`{blueBright test-job} {greenBright >} Test something`);
+    expect(writeStreams.stderrLines).toEqual([]);
+});
