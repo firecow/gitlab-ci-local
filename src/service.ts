edb1c2dfeb5f7501a24a4edf183b76a43e9dfec2
Mads Jon Nielsen <madsjon@gmail.com>
2021-06-10 08:12:38 +0200

    Implement Services (#200)
    Former-commit-id: 0ad400d3f8b66c87a61a1fbcaa5530a2d8402719

---
+++ src/service.ts
@@ -0,0 +1,41 @@
+import {Utils} from "./utils";
+import {assert} from "./asserts";
+
+export class Service {
+    private readonly serviceData: any;
+
+    constructor(serviceData: any) {
+        this.serviceData = serviceData;
+    }
+
+    public getName(expandedVariables: { [key: string]: string }): string {
+        assert(this.serviceData.name, "Service should always have an image name");
+        const servicesName = Utils.expandText(this.serviceData.name, expandedVariables);
+        return servicesName.includes(":") ? servicesName : `${servicesName}:latest`;
+    }
+
+    public getEntrypoint(): string[] | null {
+        if (!this.serviceData || !this.serviceData.entrypoint) {
+            return null;
+        }
+        assert(typeof this.serviceData.entrypoint !== "object", "services:entrypoint must be an array");
+        return this.serviceData.entrypoint;
+    }
+
+    public getCommand(expandedVariables: { [key: string]: string }): string | null {
+        if (!this.serviceData || !this.serviceData.command) {
+            return null;
+        }
+
+        return Utils.expandText(this.serviceData.command, expandedVariables);
+    }
+
+    public getAlias(expandedVariables: { [key: string]: string }): string | null {
+        if (!this.serviceData || !this.serviceData.alias) {
+            return null;
+        }
+
+        return Utils.expandText(this.serviceData.alias, expandedVariables);
+    }
+
+}
---
+++ tests/test-cases/services/integration.services.test.ts
@@ -0,0 +1,55 @@
+import {MockWriteStreams} from "../../../src/mock-write-streams";
+import {handler} from "../../../src/handler";
+import chalk from "chalk";
+
+test.concurrent("services <test-job>", async () => {
+    const writeStreams = new MockWriteStreams();
+    await handler({
+        cwd: "tests/test-cases/services",
+        job: ["test-job"],
+    }, writeStreams);
+
+    const expected = [
+        chalk`{black.bgRed  FAIL } {blueBright test-job  }`,
+    ];
+    expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
+});
+
+test.concurrent("services <build-job>", async () => {
+    const writeStreams = new MockWriteStreams();
+    await handler({
+        cwd: "tests/test-cases/services",
+        job: ["build-job"],
+    }, writeStreams);
+
+    const expected = [
+        chalk`{black.bgGreenBright  PASS } {blueBright build-job }`,
+    ];
+    expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
+});
+
+test.concurrent("services <deploy-job>", async () => {
+    const writeStreams = new MockWriteStreams();
+    await handler({
+        cwd: "tests/test-cases/services",
+        job: ["deploy-job"],
+    }, writeStreams);
+
+    const expected = [
+        chalk`{black.bgGreenBright  PASS } {blueBright deploy-job}`,
+    ];
+    expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
+});
+
+test.concurrent("services <alias-job>", async () => {
+    const writeStreams = new MockWriteStreams();
+    await handler({
+        cwd: "tests/test-cases/services",
+        job: ["alias-job"],
+    }, writeStreams);
+
+    const expected = [
+        chalk`{black.bgGreenBright  PASS } {blueBright alias-job }`,
+    ];
+    expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
+});
