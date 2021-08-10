a1525bb1db805433f55c169b3b05b082713c1d0f
Mads Jon Nielsen <madsjon@gmail.com>
2021-08-10 10:24:39 +0200

    Added integration test for CI_PROJECT_DIR (predefined variable)
    Former-commit-id: 2195c19abb30ee50dd319c786955333ba56104eb

--- tests/test-cases/predefined-variables/integration.predefined-variables.test.ts
+++ tests/test-cases/predefined-variables/integration.predefined-variables.test.ts
@@ -12,5 +12,18 @@ test("predefined-variables <test-job>", async () => {
     expect(writeStreams.stdoutLines[3]).toEqual(chalk`{blueBright test-job} {greenBright >} predefined-variables`);
     expect(writeStreams.stdoutLines[5]).toEqual(chalk`{blueBright test-job} {greenBright >} gcl-predefined-variables`);
     expect(writeStreams.stdoutLines[7]).toEqual(chalk`{blueBright test-job} {greenBright >} gcl`);
+    expect(writeStreams.stdoutLines[9]).toEqual(chalk`{blueBright test-job} {greenBright >} tests/test-cases/predefined-variables`);
+    expect(writeStreams.stderrLines).toEqual([]);
+});
+
+test("predefined-variables <test-job> --shell-isolation", async () => {
+    const writeStreams = new MockWriteStreams();
+    await handler({
+        cwd: "tests/test-cases/predefined-variables",
+        jobs: ["test-job"],
+        shellIsolation: true,
+    }, writeStreams);
+
+    expect(writeStreams.stdoutLines[9]).toEqual(chalk`{blueBright test-job} {greenBright >} tests/test-cases/predefined-variables/.gitlab-ci-local/builds/test-job`);
     expect(writeStreams.stderrLines).toEqual([]);
 });
