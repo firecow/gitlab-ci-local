9d3f388d9fb0e4cdfc1c88eabcfef6e12f81402f
Mads Jon Nielsen <madsjon@gmail.com>
2021-08-23 20:54:32 +0200

    Implement key.files via npm checksum package
    Former-commit-id: 7eec325168e3992c0999faf29c84c0ca54bd65e3

--- tests/test-cases/cache/integration.cache.test.ts
+++ tests/test-cases/cache/integration.cache.test.ts
@@ -1,11 +1,22 @@
 import {MockWriteStreams} from "../../../src/mock-write-streams";
 import {handler} from "../../../src/handler";
 
-test("cache <consume-cache> --needs", async () => {
+test.concurrent("cache <consume-cache> --needs", async () => {
     const writeStreams = new MockWriteStreams();
     await handler({
         cwd: "tests/test-cases/cache",
         job: ["consume-cache"],
+        needs: true,
+    }, writeStreams);
+
+    expect(writeStreams.stderrLines).toEqual([]);
+});
+
+test.concurrent("cache <consume-cache-key-files> --needs", async () => {
+    const writeStreams = new MockWriteStreams();
+    await handler({
+        cwd: "tests/test-cases/cache",
+        job: ["consume-cache-key-files"],
         needs: true,
     }, writeStreams);
 
