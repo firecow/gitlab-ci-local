3373b3edc5ceb9d114470fa04fa05641463e9a7e
Mads Jon Nielsen <madsjon@gmail.com>
2021-08-24 19:45:45 +0200

    Support a few more ways of using include:
    Former-commit-id: 42bc8f51e43847302dddc081da56bb4615c641b8

--- tests/test-cases/include-local/integration.include-local.test.ts
+++ tests/test-cases/include-local/integration.include-local.test.ts
@@ -2,10 +2,40 @@ import {MockWriteStreams} from "../../../src/mock-write-streams";
 import {handler} from "../../../src/handler";
 import chalk from "chalk";
 
-test("include-local <build-job>", async () => {
+test.concurrent("include-local <build-job>", async () => {
     const writeStreams = new MockWriteStreams();
     await handler({
         cwd: "tests/test-cases/include-local",
+        job: ["build-job"],
+    }, writeStreams);
+
+    const expected = [
+        chalk`{blueBright build-job} {greenBright >} Build something`,
+    ];
+    expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
+    expect(writeStreams.stderrLines).toEqual([]);
+});
+
+test.concurrent("include-local <build-job> (short-single)", async () => {
+    const writeStreams = new MockWriteStreams();
+    await handler({
+        cwd: "tests/test-cases/include-local",
+        file: ".gitlab-ci-short-single.yml",
+        job: ["build-job"],
+    }, writeStreams);
+
+    const expected = [
+        chalk`{blueBright build-job} {greenBright >} Build something`,
+    ];
+    expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
+    expect(writeStreams.stderrLines).toEqual([]);
+});
+
+test.concurrent("include-local <build-job> (short-list)", async () => {
+    const writeStreams = new MockWriteStreams();
+    await handler({
+        cwd: "tests/test-cases/include-local",
+        file: ".gitlab-ci-short-list.yml",
         job: ["build-job"],
     }, writeStreams);
 
