3843950a83dc28008aabf2cfe023e177d2472d63
Mads Jon Nielsen <madsjon@gmail.com>
2021-10-24 16:05:23 +0200

    Add failing test for parallel.matrix

--- tests/test-cases/parallel-matrix/integration.parallel-matrix.test.ts
+++ tests/test-cases/parallel-matrix/integration.parallel-matrix.test.ts
@@ -1,5 +1,6 @@
 import {MockWriteStreams} from "../../../src/mock-write-streams";
 import {handler} from "../../../src/handler";
+import chalk from "chalk";
 
 test("parallel-matrix <test-job>", async () => {
     const writeStreams = new MockWriteStreams();
@@ -8,5 +9,20 @@ test("parallel-matrix <test-job>", async () => {
         jobs: ["test-job"],
     }, writeStreams);
 
+    const expected = [
+        chalk`{blueBright build-job[foo]   } {greenBright >} NAME:'foo' 1/3`,
+        chalk`{blueBright build-job[bar]   } {greenBright >} NAME:'bar' 2/3`,
+        chalk`{blueBright build-job[bear]  } {greenBright >} NAME:'bear' 3/3`,
+
+        chalk`{blueBright test-job[foo,hey]} {greenBright >} NAME:'foo' SPIFFY:'hejsa' 1/7`,
+        chalk`{blueBright test-job[foo,dar]} {greenBright >} NAME:'foo' SPIFFY:'danling' 2/7`,
+        chalk`{blueBright test-job[foo,dun]} {greenBright >} NAME:'foo' SPIFFY:'dungling' 3/7`,
+        chalk`{blueBright test-job[bar,hey]} {greenBright >} NAME:'bar' SPIFFY:'hejsa' 4/7`,
+        chalk`{blueBright test-job[bar,dar]} {greenBright >} NAME:'bar' SPIFFY:'danling' 5/7`,
+        chalk`{blueBright test-job[bar,dun]} {greenBright >} NAME:'bar' SPIFFY:'dungling' 6/7`,
+        chalk`{blueBright test-job[bear]   } {greenBright >} NAME:'bear' SPIFFY:'' 7/7`,
+
+    ];
+    expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
     expect(writeStreams.stderrLines.length).toEqual(0);
 });
