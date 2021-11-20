5fdb4cb1183a65cd0b2e50277edde517a275ded9
Mads Jon Nielsen <madsjon@gmail.com>
2021-11-20 16:04:51 +0100

    Fix test

--- tests/test-cases/parallel-matrix/integration.parallel-matrix.test.ts
+++ tests/test-cases/parallel-matrix/integration.parallel-matrix.test.ts
@@ -14,12 +14,12 @@ test("parallel-matrix <test-job>", async () => {
         chalk`{blueBright build-job[bar]   } {greenBright >} NAME:'bar' 2/3`,
         chalk`{blueBright build-job[bear]  } {greenBright >} NAME:'bear' 3/3`,
 
-        chalk`{blueBright test-job[foo,hey]} {greenBright >} NAME:'foo' SPIFFY:'hejsa' 1/7`,
-        chalk`{blueBright test-job[foo,dar]} {greenBright >} NAME:'foo' SPIFFY:'danling' 2/7`,
-        chalk`{blueBright test-job[foo,dun]} {greenBright >} NAME:'foo' SPIFFY:'dungling' 3/7`,
-        chalk`{blueBright test-job[bar,hey]} {greenBright >} NAME:'bar' SPIFFY:'hejsa' 4/7`,
-        chalk`{blueBright test-job[bar,dar]} {greenBright >} NAME:'bar' SPIFFY:'danling' 5/7`,
-        chalk`{blueBright test-job[bar,dun]} {greenBright >} NAME:'bar' SPIFFY:'dungling' 6/7`,
+        chalk`{blueBright test-job[foo,hey]} {greenBright >} NAME:'foo' SPIFFY:'hey' 1/7`,
+        chalk`{blueBright test-job[foo,dar]} {greenBright >} NAME:'foo' SPIFFY:'dar' 2/7`,
+        chalk`{blueBright test-job[foo,dun]} {greenBright >} NAME:'foo' SPIFFY:'dun' 3/7`,
+        chalk`{blueBright test-job[bar,hey]} {greenBright >} NAME:'bar' SPIFFY:'hey' 4/7`,
+        chalk`{blueBright test-job[bar,dar]} {greenBright >} NAME:'bar' SPIFFY:'dar' 5/7`,
+        chalk`{blueBright test-job[bar,dun]} {greenBright >} NAME:'bar' SPIFFY:'dun' 6/7`,
         chalk`{blueBright test-job[bear]   } {greenBright >} NAME:'bear' SPIFFY:'' 7/7`,
 
     ];
