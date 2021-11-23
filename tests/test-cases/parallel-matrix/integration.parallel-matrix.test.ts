6f9ad8214ccf7f107abab48b900283f9694b51d2
Mads Jon Nielsen <madsjon@gmail.com>
2021-11-23 15:38:12 +0100

    Changed the test a little

--- tests/test-cases/parallel-matrix/integration.parallel-matrix.test.ts
+++ tests/test-cases/parallel-matrix/integration.parallel-matrix.test.ts
@@ -12,15 +12,15 @@ test("parallel-matrix <test-job>", async () => {
     const expected = [
         chalk`{blueBright build-job[foo]   } {greenBright >} NAME:'foo' 1/3`,
         chalk`{blueBright build-job[bar]   } {greenBright >} NAME:'bar' 2/3`,
-        chalk`{blueBright build-job[bear]  } {greenBright >} NAME:'bear' 3/3`,
+        chalk`{blueBright build-job[beb]   } {greenBright >} NAME:'beb' 3/3`,
 
-        chalk`{blueBright test-job[foo,hey]} {greenBright >} NAME:'foo' SPIFFY:'hey' 1/7`,
-        chalk`{blueBright test-job[foo,dar]} {greenBright >} NAME:'foo' SPIFFY:'dar' 2/7`,
-        chalk`{blueBright test-job[foo,dun]} {greenBright >} NAME:'foo' SPIFFY:'dun' 3/7`,
-        chalk`{blueBright test-job[bar,hey]} {greenBright >} NAME:'bar' SPIFFY:'hey' 4/7`,
-        chalk`{blueBright test-job[bar,dar]} {greenBright >} NAME:'bar' SPIFFY:'dar' 5/7`,
-        chalk`{blueBright test-job[bar,dun]} {greenBright >} NAME:'bar' SPIFFY:'dun' 6/7`,
-        chalk`{blueBright test-job[bear]   } {greenBright >} NAME:'bear' SPIFFY:'' 7/7`,
+        chalk`{blueBright test-job[foo,hey]} {greenBright >} NAME:'foo' ENV:'hey' 1/7`,
+        chalk`{blueBright test-job[foo,dar]} {greenBright >} NAME:'foo' ENV:'dar' 2/7`,
+        chalk`{blueBright test-job[foo,dun]} {greenBright >} NAME:'foo' ENV:'dun' 3/7`,
+        chalk`{blueBright test-job[bar,hey]} {greenBright >} NAME:'bar' ENV:'hey' 4/7`,
+        chalk`{blueBright test-job[bar,dar]} {greenBright >} NAME:'bar' ENV:'dar' 5/7`,
+        chalk`{blueBright test-job[bar,dun]} {greenBright >} NAME:'bar' ENV:'dun' 6/7`,
+        chalk`{blueBright test-job[beb]    } {greenBright >} NAME:'beb' ENV:'' 7/7`,
 
     ];
     expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
