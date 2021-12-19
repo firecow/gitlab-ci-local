30dae5059c62f116f9bfa5f1f9ad7d475ce2a266
Mads Jon Nielsen <madsjon@gmail.com>
2021-12-19 16:06:20 +0100

    Try mocking project-file-ref includes

--- tests/test-cases/include-project-file-ref-with-inner-local/integration.include-project-file-ref-with-inner-local.test.ts
+++ tests/test-cases/include-project-file-ref-with-inner-local/integration.include-project-file-ref-with-inner-local.test.ts
@@ -5,7 +5,7 @@ import {initSpawnSpy} from "../../mocks/utils.mock";
 import {WhenStatics} from "../../mocks/when-statics";
 import fs from "fs-extra";
 
-test("include-project-file-ref <deploy-job>", async () => {
+test("include-project-file-ref-with-inner-local", async () => {
     await fs.rm("tests/test-cases/include-project-file-ref/.gitlab-ci-local", {recursive: true, force: true});
     const writeStreams = new MockWriteStreams();
     const spyGitRemote = {
@@ -13,21 +13,21 @@ test("include-project-file-ref <deploy-job>", async () => {
         returnValue: {stdout: "origin\tgit@gitlab.com:gcl/test-hest.git (fetch)\norigin\tgit@gitlab.com:gcl/test-hest.git (push)\n"},
     };
     const target = ".gitlab-ci-local/includes/gitlab.com/firecow/gitlab-ci-local-includes/master/";
-    const spyGitArchive = {
-        cmd: `git archive --remote=ssh://git@gitlab.com:22/firecow/gitlab-ci-local-includes.git master .gitlab-module.yml | tar -f - -xC ${target}`,
+    const spyGitArchive1 = {
+        cmd: `git archive --remote=ssh://git@gitlab.com:22/firecow/gitlab-ci-local-includes.git include-string-list .gitlab-module-with-local.yml | tar -f - -xC ${target}`,
         returnValue: {output: ""},
     };
-    initSpawnSpy([...WhenStatics.all, spyGitRemote, spyGitArchive]);
-    const mock = "tests/test-cases/include-project-file-ref/mock-gitlab-module.yml";
-    const mockTarget = "tests/test-cases/include-project-file-ref/.gitlab-ci-local/includes/gitlab.com/firecow/gitlab-ci-local-includes/master/.gitlab-module.yml";
-    await fs.ensureFile(mockTarget);
-    await fs.copyFile(mock, mockTarget);
+    const spyGitArchive2 = {
+        cmd: `git archive --remote=ssh://git@gitlab.com:22/firecow/gitlab-ci-local-includes.git include-string-list .gitlab-local.yml | tar -f - -xC ${target}`,
+        returnValue: {output: ""},
+    };
+    initSpawnSpy([...WhenStatics.all, spyGitRemote, spyGitArchive1, spyGitArchive2]);
     await handler({
-        cwd: "tests/test-cases/include-project-file-ref",
-        job: ["deploy-job"],
+        cwd: "tests/test-cases/include-project-file-ref-with-inner-local",
     }, writeStreams);
 
     const expected = [
+        chalk`{blueBright test-job  } {greenBright >} Test something`,
         chalk`{blueBright deploy-job} {greenBright >} Deploy something`,
     ];
     expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
