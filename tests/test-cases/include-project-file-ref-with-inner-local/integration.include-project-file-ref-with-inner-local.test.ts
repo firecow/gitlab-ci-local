e7919f657d5dd3263f85cde7fd942708e7679df4
Mads Jon Nielsen <madsjon@gmail.com>
2021-12-19 16:19:46 +0100

    Fix inner local include test

--- tests/test-cases/include-project-file-ref-with-inner-local/integration.include-project-file-ref-with-inner-local.test.ts
+++ tests/test-cases/include-project-file-ref-with-inner-local/integration.include-project-file-ref-with-inner-local.test.ts
@@ -6,13 +6,14 @@ import {WhenStatics} from "../../mocks/when-statics";
 import fs from "fs-extra";
 
 test("include-project-file-ref-with-inner-local", async () => {
-    await fs.rm("tests/test-cases/include-project-file-ref/.gitlab-ci-local", {recursive: true, force: true});
+    const cwd = "tests/test-cases/include-project-file-ref-with-inner-local";
+    await fs.rm(`${cwd}/.gitlab-ci-local/`, {recursive: true, force: true});
     const writeStreams = new MockWriteStreams();
     const spyGitRemote = {
         cmd: "git remote -v",
         returnValue: {stdout: "origin\tgit@gitlab.com:gcl/test-hest.git (fetch)\norigin\tgit@gitlab.com:gcl/test-hest.git (push)\n"},
     };
-    const target = ".gitlab-ci-local/includes/gitlab.com/firecow/gitlab-ci-local-includes/master/";
+    const target = ".gitlab-ci-local/includes/gitlab.com/firecow/gitlab-ci-local-includes/include-string-list/";
     const spyGitArchive1 = {
         cmd: `git archive --remote=ssh://git@gitlab.com:22/firecow/gitlab-ci-local-includes.git include-string-list .gitlab-module-with-local.yml | tar -f - -xC ${target}`,
         returnValue: {output: ""},
@@ -22,9 +23,18 @@ test("include-project-file-ref-with-inner-local", async () => {
         returnValue: {output: ""},
     };
     initSpawnSpy([...WhenStatics.all, spyGitRemote, spyGitArchive1, spyGitArchive2]);
-    await handler({
-        cwd: "tests/test-cases/include-project-file-ref-with-inner-local",
-    }, writeStreams);
+
+    let mock = `${cwd}/mock-gitlab-module-with-local.yml`;
+    let mockTarget = `${cwd}/.gitlab-ci-local/includes/gitlab.com/firecow/gitlab-ci-local-includes/include-string-list/.gitlab-module-with-local.yml`;
+    await fs.ensureFile(mockTarget);
+    await fs.copyFile(mock, mockTarget);
+
+    mock = `${cwd}/mock-gitlab-local.yml`;
+    mockTarget = `${cwd}/.gitlab-ci-local/includes/gitlab.com/firecow/gitlab-ci-local-includes/include-string-list/.gitlab-local.yml`;
+    await fs.ensureFile(mockTarget);
+    await fs.copyFile(mock, mockTarget);
+
+    await handler({cwd}, writeStreams);
 
     const expected = [
         chalk`{blueBright test-job  } {greenBright >} Test something`,
