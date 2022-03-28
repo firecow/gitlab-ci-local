bee78dd2265bc4d215e63eb97e4f18c232139c24
Mads Jon Nielsen <madsjon@gmail.com>
2022-03-28 17:32:05 +0200

    Add unit test for remote variables
    Former-commit-id: 9e2d03a662e50beccacf5252794a7de0fe582960

---
+++ tests/test-cases/remote-variables-file/integration.project-variables-file.test.ts
@@ -0,0 +1,28 @@
+import {MockWriteStreams} from "../../../src/mock-write-streams";
+import {handler} from "../../../src/handler";
+import chalk from "chalk";
+import {initSpawnSpy, initBashSpy} from "../../mocks/utils.mock";
+import {WhenStatics} from "../../mocks/when-statics";
+
+beforeAll(() => {
+    const spyGitArchive = {
+        cmd: "git archive --remote=git@gitlab.com:example/firecow.git main variables.yml | tar -xO variables.yml",
+        returnValue: {stdout: "---\nglobal:\n  SOMEVARIABLE: very-special-value"},
+    };
+    initSpawnSpy([...WhenStatics.all]);
+    initBashSpy([spyGitArchive]);
+});
+
+test.concurrent("remote-variables-file <test-job>", async () => {
+    const writeStreams = new MockWriteStreams();
+    await handler({
+        cwd: "tests/test-cases/remote-variables-file",
+        job: ["test-job"],
+        remoteVariables: "git@gitlab.com:example/firecow.git=variables.yml=main",
+    }, writeStreams);
+
+    const expected = [
+        chalk`{blueBright test-job} {greenBright >} very-special-value`,
+    ];
+    expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
+});
