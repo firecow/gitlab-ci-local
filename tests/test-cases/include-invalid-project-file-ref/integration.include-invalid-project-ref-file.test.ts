3cf34f35a718c360c8376d4492651e77f3f53f75
Mads Jon Nielsen <madsjon@gmail.com>
2022-05-05 10:44:01 +0200

    Don't await on timeout, just mock a rejection (#453)
    Former-commit-id: 456f8f347f1216efad9d993a93ad848946957fbc

--- tests/test-cases/include-invalid-project-file-ref/integration.include-invalid-project-ref-file.test.ts
+++ tests/test-cases/include-invalid-project-file-ref/integration.include-invalid-project-ref-file.test.ts
@@ -1,15 +1,24 @@
 import {MockWriteStreams} from "../../../src/mock-write-streams";
 import {handler} from "../../../src/handler";
 import {assert} from "../../../src/asserts";
-import {initSpawnSpy} from "../../mocks/utils.mock";
+import {initBashSpyReject, initSpawnSpy} from "../../mocks/utils.mock";
 import {WhenStatics} from "../../mocks/when-statics";
-
-beforeAll(() => {
-    initSpawnSpy(WhenStatics.all);
-});
 
 test("include-invalid-project-file-ref", async () => {
     try {
+
+        const target = ".gitlab-ci-local/includes/gitlab.com/firecow/gitlab-ci-local-includes/master/";
+        const spyGitArchive = {
+            cmd: `git archive --remote=ssh://git@gitlab.com:22/firecow/gitlab-ci-local-includes.git master .gitlab-modue.yml | tar -f - -xC ${target}`,
+            rejection: "Project include could not be fetched { project: firecow/gitlab-ci-local-includes, ref: master, file: .gitlab-modue.yml }",
+        };
+        initBashSpyReject([spyGitArchive]);
+        const spyGitRemote = {
+            cmdArgs: ["git", "remote", "-v"],
+            returnValue: {stdout: "origin\tgit@gitlab.com:gcl/test-hest.git (fetch)\norigin\tgit@gitlab.com:gcl/test-hest.git (push)\n"},
+        };
+
+        initSpawnSpy([...WhenStatics.all, spyGitRemote]);
         const writeStreams = new MockWriteStreams();
         await handler({
             cwd: "tests/test-cases/include-invalid-project-file-ref",
