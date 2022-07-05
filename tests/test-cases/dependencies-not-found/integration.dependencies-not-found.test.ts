a4174af4647bd539ba37f6f163cd513fcb8054e3
Mads Jon Nielsen <madsjon@gmail.com>
2022-07-05 11:05:42 +0200

    Validate dependencies instructions in validator.ts (#535)

--- tests/test-cases/dependencies-not-found/integration.dependencies-not-found.test.ts
+++ tests/test-cases/dependencies-not-found/integration.dependencies-not-found.test.ts
@@ -1,24 +1,24 @@
 import {WriteStreamsMock} from "../../../src/write-streams-mock";
 import {handler} from "../../../src/handler";
 import chalk from "chalk";
-import {assert} from "../../../src/asserts";
 import {initSpawnSpy} from "../../mocks/utils.mock";
 import {WhenStatics} from "../../mocks/when-statics";
+import {assert} from "../../../src/asserts";
 
 beforeAll(() => {
     initSpawnSpy(WhenStatics.all);
 });
 
-test("needs-invalid-stage <build-job> --needs", async () => {
+test("dependencies-not-found <test-job>", async () => {
     const writeStreams = new WriteStreamsMock();
     try {
         await handler({
-            cwd: "tests/test-cases/needs-invalid-stage",
-            job: ["build-job"],
+            cwd: "tests/test-cases/dependencies-not-found",
+            job: ["test-job"],
         }, writeStreams);
         expect(true).toBe(false);
     } catch (e) {
         assert(e instanceof Error, "e is not instanceof Error");
-        expect(e.message).toBe(chalk`needs: [{blueBright test-job}] for {blueBright build-job} is in a future stage`);
+        expect(e.message).toBe(chalk`dependencies: [{blueBright invalid}] for {blueBright test-job} cannot be found`);
     }
 });
