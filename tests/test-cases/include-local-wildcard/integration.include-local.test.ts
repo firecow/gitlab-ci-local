179f05164d33cf4d4c4c72c8e83abd2dcba2259c
Alexander Matthiesen <alexander.matthiesen@extern.dkbcodefactory.com>
2022-08-08 22:23:49 +0200

    feat(197): Adding tests for include wildcard
    Former-commit-id: ae4bc88b5cb5d888c52c4e8cc3720e0938474096

---
+++ tests/test-cases/include-local-wildcard/integration.include-local.test.ts
@@ -0,0 +1,22 @@
+import {WriteStreamsMock} from "../../../src/write-streams-mock";
+import {handler} from "../../../src/handler";
+import chalk from "chalk";
+import {initSpawnSpy} from "../../mocks/utils.mock";
+import {WhenStatics} from "../../mocks/when-statics";
+
+beforeAll(() => {
+    initSpawnSpy(WhenStatics.all);
+});
+
+test("include-local-wildcard <build-job>", async () => {
+    const writeStreams = new WriteStreamsMock();
+    await handler({
+        cwd: "tests/test-cases/include-local-wildcard",
+        file: ".gitlab-ci.yml",
+    }, writeStreams);
+
+    const output = writeStreams.stdoutLines.join();
+    expect(output).toContain("build-images executed!");
+    expect(output).toContain("cache-repo executed!");
+    expect(output).toContain("docs executed!");
+});
