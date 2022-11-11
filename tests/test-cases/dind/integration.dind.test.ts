6b851156437f7cd2a63740a9ebe7d86826ea4ec1
Mads Jon Nielsen <madsjon@gmail.com>
2022-11-11 09:09:42 +0100

    Added integration test for issue 616

---
+++ tests/test-cases/dind/integration.dind.test.ts
@@ -0,0 +1,25 @@
+import {WriteStreamsMock} from "../../../src/write-streams-mock";
+import {handler} from "../../../src/handler";
+import chalk from "chalk";
+import {initSpawnSpy} from "../../mocks/utils.mock";
+import {WhenStatics} from "../../mocks/when-statics";
+
+jest.setTimeout(30000);
+
+beforeAll(() => {
+    initSpawnSpy(WhenStatics.all);
+});
+
+test("dind <test-job> --needs", async () => {
+    const writeStreams = new WriteStreamsMock();
+    await handler({
+        cwd: "tests/test-cases/dind",
+        job: ["test-job"],
+        needs: true
+    }, writeStreams);
+
+    const expected = [
+        chalk`{blueBright test-job} {greenBright >} TestJobDIND`,
+    ];
+    expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
+});
