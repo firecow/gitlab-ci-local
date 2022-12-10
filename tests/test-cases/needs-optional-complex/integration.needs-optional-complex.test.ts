91f936ec8612ad77859bb4a2837d7ddf77b73cd9
Alexander Matthiesen <53814939+alexander-matthiesen@users.noreply.github.com>
2022-12-10 18:08:15 +0100

    fix(689): Adding case to execute pipelines with absent optional jobs (#690)
    * test(689): Adding simple needs-optional test
    
    * test(689): Adding needs-optional-complex test
    
    * fix(689): Adding case for skipping optional jobs with when: never config
    
    Co-authored-by: Alexander Matthiesen <alexander.matthiesen2@dkb.de>
    Former-commit-id: 1085ddd0a1abdb7c154da80d5e919f68e2511b22

---
+++ tests/test-cases/needs-optional-complex/integration.needs-optional-complex.test.ts
@@ -0,0 +1,23 @@
+import {WriteStreamsMock} from "../../../src/write-streams-mock";
+import {handler} from "../../../src/handler";
+import {initSpawnSpy} from "../../mocks/utils.mock";
+import {WhenStatics} from "../../mocks/when-statics";
+
+beforeAll(() => {
+    initSpawnSpy(WhenStatics.all);
+});
+
+test("needs-optional-complex", async () => {
+    const writeStreams = new WriteStreamsMock();
+    await handler({
+        cwd: "tests/test-cases/needs-optional-complex",
+        needs: true,
+    }, writeStreams);
+
+    const output = writeStreams.stdoutLines.join();
+    console.log(output);
+
+    expect(output).toContain("Validated!");
+    expect(output).toContain("Job executed!");
+    expect(output).not.toContain("Job skipped!");
+});
