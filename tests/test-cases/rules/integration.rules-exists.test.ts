46cfdde766ae8e89324c819426c95efbe4398422
Alexander Matthiesen <>
2023-01-02 11:32:30 +0100

    bug: Rules not validated correctly

---
+++ tests/test-cases/rules/integration.rules-exists.test.ts
@@ -0,0 +1,31 @@
+import {WriteStreamsMock} from "../../../src/write-streams-mock";
+import {handler} from "../../../src/handler";
+import {initSpawnSpy} from "../../mocks/utils.mock";
+import {WhenStatics} from "../../mocks/when-statics";
+
+beforeAll(() => {
+    initSpawnSpy(WhenStatics.all);
+});
+
+test("rules-when test=true", async () => {
+    const writeStreams = new WriteStreamsMock();
+    await handler({
+        cwd: "tests/test-cases/rules",
+        variables: { TEST: true },
+        listCsv: true,
+    }, writeStreams);
+
+    const output = writeStreams.stdoutLines.join("\n");
+    expect(output).toContain('test-job;"";build;on_success;false;[]');
+});
+
+test("rules-when test=undefined", async () => {
+    const writeStreams = new WriteStreamsMock();
+    await handler({
+        cwd: "tests/test-cases/rules",
+        listCsv: true,
+    }, writeStreams);
+
+    const output = writeStreams.stdoutLines.join("\n");
+    expect(output).toContain('test-job;"";build;manual;false;[]');
+});
