2242bbd976d88a013bc469cc401c90489e2e736c
Przemysław Broniszewski <przemek@broniszewski.net>
2023-01-09 18:33:26 +0100

    issue-717 (https://github.com/firecow/gitlab-ci-local/issues/717) Add support for cache:when (https://docs.gitlab.com/ee/ci/yaml/index.html#cachewhen)

--- src/cache-entry.ts
+++ src/cache-entry.ts
@@ -4,11 +4,13 @@ export class CacheEntry {
     public readonly policy: "pull" | "pull-push" | "push";
     public readonly key: string | {files: string[]};
     public readonly paths: string[];
+    public readonly when: "on_success" | "on_failure" | "always";
 
-    constructor (key: string | {files: string[]}, paths: string[], policy: "pull" | "pull-push" | "push") {
+    constructor (key: string | {files: string[]}, paths: string[], policy: "pull" | "pull-push" | "push", when: "on_success" | "on_failure" | "always") {
         this.key = key;
         this.policy = policy;
         this.paths = paths;
+        this.when = when;
     }
 
     async getUniqueCacheName (cwd: string, env: {[key: string]: string}) {
