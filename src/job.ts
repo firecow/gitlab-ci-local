5365f3f261d999830b4a5b8c956129cca613c72c
Mads Jon Nielsen <mjn@cego.dk>
2023-05-07 14:33:05 +0200

    No need to return null here

--- src/job.ts
+++ src/job.ts
@@ -539,7 +539,7 @@ export class Job {
         if (!this.injectSSHAgent) {
             return "";
         }
-        if (process.platform === "darwin" || (/^darwin/.exec(process.env.OSTYPE ?? "") ?? null)) {
+        if (process.platform === "darwin" || /^darwin/.exec(process.env.OSTYPE ?? "")) {
             return "--env SSH_AUTH_SOCK=/run/host-services/ssh-auth.sock -v /run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock";
         }
         return `--env SSH_AUTH_SOCK=${process.env.SSH_AUTH_SOCK} -v ${process.env.SSH_AUTH_SOCK}:${process.env.SSH_AUTH_SOCK}`;
