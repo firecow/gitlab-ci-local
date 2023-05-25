13b186d5ce2b79d598925b39542ccaeb27eeb067
Mads Jon Nielsen <madsjon@gmail.com>
2023-05-25 10:48:17 +0200

    Remember to cleanupResources on exit

--- src/handler.ts
+++ src/handler.ts
@@ -53,6 +53,10 @@ export async function handler (args: any, writeStreams: WriteStreams): Promise<R
         }
     });
 
+    process.on("exit", (_: string, code: number) => {
+        cleanupResources(parser).finally(process.exit(code));
+    });
+
     process.on("SIGINT", (_: string, code: number) => {
         cleanupResources(parser).finally(process.exit(code));
     });
