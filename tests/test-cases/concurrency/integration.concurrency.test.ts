a61fc4e376d553e64840ffeb105df45dd0fa002c
naweiss <naweiss@users.noreply.github.com>
2023-07-09 15:07:27 -0400

    Add tests

---
+++ tests/test-cases/concurrency/integration.concurrency.test.ts
@@ -0,0 +1,29 @@
+import {WriteStreamsMock} from "../../../src/write-streams";
+import {handler} from "../../../src/handler";
+import {initSpawnSpy} from "../../mocks/utils.mock";
+import {WhenStatics} from "../../mocks/when-statics";
+import assert from "assert";
+
+beforeAll(() => {
+    initSpawnSpy(WhenStatics.all);
+});
+
+test("--jobs 1 - should run sequentially", async () => {
+    const writeStreams = new WriteStreamsMock();
+    await handler({
+        cwd: "tests/test-cases/concurrency",
+        jobs: 1,
+    }, writeStreams);
+
+    // tip: use `cat __snapshots__/*` to inspect the results
+    expect(writeStreams.stdoutLines.join("\n").replace(/[0-9.]+ m?s/g, "1 ms")).toMatchSnapshot();
+});
+
+test("--jobs not set", async () => {
+    const writeStreams = new WriteStreamsMock();
+    await handler({
+        cwd: "tests/test-cases/concurrency",
+    }, writeStreams);
+
+    expect(writeStreams.stdoutLines.join("\n").replace(/[0-9.]+ m?s/g, "1 ms")).toMatchSnapshot();
+});
