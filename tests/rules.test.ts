9fa0cb8f0f45a11f0118dc4e682303924a6df759
Mads Jon Nielsen <madsjon@gmail.com>
2023-07-14 23:14:54 +0200

    Add failing test for issue 932

--- tests/rules.test.ts
+++ tests/rules.test.ts
@@ -274,7 +274,16 @@ test("https://github.com/firecow/gitlab-ci-local/issues/609", () => {
     const rules = [
         {if: "$CI_COMMIT_REF_NAME =~ $PROD_REF", when: "manual"},
     ];
-    const variables = {CI_COMMIT_REF_NAME: "main", PROD_REF: "/^(master|main)$/"};
+    const variables = {CI_COMMIT_REF_NAME: "main", PROD_REF: "/^((.*)master|main)$/"};
+    const rulesResult = Utils.getRulesResult({cwd: "", rules, variables});
+    expect(rulesResult).toEqual({when: "manual", allowFailure: false, variables: undefined});
+});
+
+test("https://github.com/firecow/gitlab-ci-local/issues/932", () => {
+    const rules = [
+        {if: "$CI_COMMIT_TAG =~ $REGEX", when: "manual"},
+    ];
+    const variables = {CI_COMMIT_TAG: "1.10", REGEX: "/^(\\d+(\\.\\d+)*|latest)$/"};
     const rulesResult = Utils.getRulesResult({cwd: "", rules, variables});
     expect(rulesResult).toEqual({when: "manual", allowFailure: false, variables: undefined});
 });
