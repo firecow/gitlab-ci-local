d590ce14de6b6c7676902d6f79886336456861b9
Dimitri Papoutsis <dimitrios.papoutsis@dfs.de>
2023-08-18 00:34:21 +0200

    added test: upload of artifacts after after-script

---
+++ tests/test-cases/artifacts-after-afterscript/integration.artifacts-after-afterscript.test.ts
@@ -0,0 +1,21 @@
+import {WriteStreamsMock} from "../../../src/write-streams";
+import {handler} from "../../../src/handler";
+import {initSpawnSpy} from "../../mocks/utils.mock";
+import {WhenStatics} from "../../mocks/when-statics";
+import fs from "fs-extra";
+
+beforeAll(() => {
+    initSpawnSpy(WhenStatics.all);
+});
+
+test("after-script <artifact-job>: after scripts are executed before uploading artifacts", async () => {
+    const writeStreams = new WriteStreamsMock();
+    await handler({
+        cwd: "tests/test-cases/artifacts-after-afterscript",
+        job: ["artifact-job"],
+    }, writeStreams);
+
+    expect(await fs.pathExists("tests/test-cases/artifacts-after-afterscript/.gitlab-ci-local/artifacts/artifact-job/logs/foo.log")).toEqual(true);
+    expect(await fs.pathExists("tests/test-cases/artifacts-after-afterscript/.gitlab-ci-local/artifacts/artifact-job/logs/foo.badlog")).toEqual(false);
+    await fs.promises.rm("tests/test-cases/after-script/logs", {force: true, recursive: true});
+});
