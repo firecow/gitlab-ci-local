838afdcbbd451ec25762d1f9e9308f0932a76915
Mads Jon Nielsen <madsjon@gmail.com>
2023-11-24 18:46:00 +0100

    Closes #994 (#1041)

---
+++ tests/test-cases/artifacts-dotenv/integration.artifacts-dotenv.test.ts
@@ -0,0 +1,19 @@
+import {WriteStreamsMock} from "../../../src/write-streams";
+import {handler} from "../../../src/handler";
+import {initSpawnSpy} from "../../mocks/utils.mock";
+import {WhenStatics} from "../../mocks/when-statics";
+
+beforeAll(() => {
+    initSpawnSpy(WhenStatics.all);
+});
+
+test("artifacts-dotenv <use-image-ref> --needs", async () => {
+    const writeStreams = new WriteStreamsMock();
+    await handler({
+        cwd: "tests/test-cases/artifacts-dotenv",
+        job: ["use-image-ref"],
+        needs: true,
+    }, writeStreams);
+
+    expect(writeStreams.stderrLines.join("\n")).not.toMatch(/FAIL/);
+});
