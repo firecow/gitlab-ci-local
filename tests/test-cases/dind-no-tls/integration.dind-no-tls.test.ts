8e96b17248ca3a3686d66c79e429309f0e948f0d
Mads Jon Nielsen <madsjon@gmail.com>
2023-12-16 23:40:11 +0100

    Add failing dind-no-tls test

---
+++ tests/test-cases/dind-no-tls/integration.dind-no-tls.test.ts
@@ -0,0 +1,27 @@
+import {WriteStreamsMock} from "../../../src/write-streams";
+import {handler} from "../../../src/handler";
+import chalk from "chalk";
+import {initSpawnSpy} from "../../mocks/utils.mock";
+import {WhenStatics} from "../../mocks/when-statics";
+
+jest.setTimeout(60000);
+
+beforeAll(() => {
+    initSpawnSpy(WhenStatics.all);
+});
+
+test("dind-no-tls <test-job> --needs", async () => {
+    const writeStreams = new WriteStreamsMock();
+    await handler({
+        cwd: "tests/test-cases/dind-tls",
+        job: ["test-job"],
+        needs: true,
+        privileged: true,
+    }, writeStreams);
+
+    const expected = [
+        chalk`{blueBright test-job} {greenBright >} TestJobDIND`,
+        chalk`{blueBright test-job} {greenBright >} Touchme`,
+    ];
+    expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
+});
