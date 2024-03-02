5f3861780cb31d6706f7413d5326d5d66232a528
ANGkeith <angkeith@hotmail.sg>
2024-03-02 15:05:35 +0800

    feat: add max nested include limits

--- tests/test-cases/include-local-wildcard/integration.include-local.test.ts
+++ tests/test-cases/include-local-wildcard/integration.include-local.test.ts
@@ -1,21 +1,26 @@
 import {WriteStreamsMock} from "../../../src/write-streams";
 import {handler} from "../../../src/handler";
 import {initSpawnSpy} from "../../mocks/utils.mock";
+import assert, {AssertionError} from "assert";
 import {WhenStatics} from "../../mocks/when-statics";
+import chalk from "chalk";
 
 beforeAll(() => {
     initSpawnSpy(WhenStatics.all);
 });
 
 test("include-local-wildcard <build-job>", async () => {
-    const writeStreams = new WriteStreamsMock();
-    await handler({
-        cwd: "tests/test-cases/include-local-wildcard",
-        file: ".gitlab-ci.yml",
-    }, writeStreams);
+    try {
+        const writeStreams = new WriteStreamsMock();
+        await handler({
+            cwd: "tests/test-cases/include-local-wildcard",
+            file: ".gitlab-ci.yml",
+        }, writeStreams);
+    } catch (e: any) {
+        assert(e instanceof AssertionError, "e is not instanceof AssertionError");
+        expect(e.message).toEqual(chalk`This GitLab CI configuration is invalid: Maximum of {blueBright 150} nested includes are allowed!.`);
+        return;
+    }
 
-    const output = writeStreams.stdoutLines.join();
-    expect(output).toContain("build-images executed!");
-    expect(output).toContain("cache-repo executed!");
-    expect(output).toContain("docs executed!");
+    throw new Error("Error is expected but not thrown/caught");
 });
