40e17bebf8e50bf90705e8e7fa30c6369d62fa1b
ANGkeith <angkeith@hotmail.sg>
2024-04-15 23:07:07 +0800

    refactor: rewrite unit-test to make it less dependent the stdout/stderr length

--- tests/test-cases/concurrency/integration.concurrency.test.ts
+++ tests/test-cases/concurrency/integration.concurrency.test.ts
@@ -15,16 +15,16 @@ test("--concurrency 1 - should run sequentially", async () => {
         concurrency: 1,
     }, writeStreams);
 
-    const jobOneIndex = writeStreams.stdoutLines.findIndex((line) => {
-        return line === chalk`{blueBright one} {green $ sleep 1}`;
+    const jobOneFinishedIndex = writeStreams.stdoutLines.findIndex((line) => {
+        return line.startsWith(chalk`{blueBright one} {magentaBright finished}`);
     });
 
-    const jobTwoIndex = writeStreams.stdoutLines.findIndex((line) => {
-        return line === chalk`{blueBright two} {green $ sleep 1}`;
+    const jobTwoStartingIndex = writeStreams.stdoutLines.findIndex((line) => {
+        return line.startsWith(chalk`{blueBright two} {magentaBright starting}`);
     });
 
-    expect(jobOneIndex).toEqual(1);
-    expect(jobTwoIndex).toEqual(4);
+    // job two should start only after job one is finished
+    expect(jobOneFinishedIndex + 1 === jobTwoStartingIndex).toBe(true);
 });
 
 test("--concurrency not set", async () => {
@@ -33,5 +33,24 @@ test("--concurrency not set", async () => {
         cwd: "tests/test-cases/concurrency",
     }, writeStreams);
 
-    expect(writeStreams.stderrLines.length).toEqual(3);
+    const jobOneStartingIndex = writeStreams.stdoutLines.findIndex((line) => {
+        return line.startsWith(chalk`{blueBright one} {magentaBright starting}`);
+    });
+
+    const jobOneFinishedIndex = writeStreams.stdoutLines.findIndex((line) => {
+        return line.startsWith(chalk`{blueBright one} {magentaBright finished}`);
+    });
+
+    const jobTwoStartingIndex = writeStreams.stdoutLines.findIndex((line) => {
+        return line.startsWith(chalk`{blueBright two} {magentaBright starting}`);
+    });
+
+    const jobTwoFinishedIndex = writeStreams.stdoutLines.findIndex((line) => {
+        return line.startsWith(chalk`{blueBright two} {magentaBright finished}`);
+    });
+
+    // job two should start before job one is finished
+    expect(jobOneFinishedIndex > jobTwoStartingIndex).toBe(true);
+    // job one should start before job two is finished
+    expect(jobTwoFinishedIndex > jobOneStartingIndex).toBe(true);
 });
