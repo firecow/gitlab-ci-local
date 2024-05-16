4f0d92f115e350d7557add36f12effea72e1dc35
Nicolas D'Amours <nicolas@folkshr.com>
2024-05-16 11:33:08 -0400

    [FEATURE] #1217
    Removed "AMD_64" from runner preset enum.

--- src/gitlab-preset.ts
+++ src/gitlab-preset.ts
@@ -1,25 +1,25 @@
 export enum GitlabRunnerPreset {
-    AMD_64_SMALL = "saas-linux-small",
-    AMD_64_MEDIUM = "saas-linux-medium",
-    AMD_64_LARGE = "saas-linux-large",
-    AMD_64_XLARGE = "saas-linux-xlarge",
-    AMD_64_2XLARGE = "saas-linux-2xlarge",
+    SMALL = "saas-linux-small",
+    MEDIUM = "saas-linux-medium",
+    LARGE = "saas-linux-large",
+    XLARGE = "saas-linux-xlarge",
+    TWO_XLARGE = "saas-linux-2xlarge",
 }
 
 export const GitlabRunnerPresetValues: string[] = Object.values(GitlabRunnerPreset);
 
 export const GitlabRunnerMemoryPresetValue: Record<string, number> = {
-    [GitlabRunnerPreset.AMD_64_SMALL]: 8 * 1024,
-    [GitlabRunnerPreset.AMD_64_MEDIUM]: 16 * 1024,
-    [GitlabRunnerPreset.AMD_64_LARGE]: 32 * 1024,
-    [GitlabRunnerPreset.AMD_64_XLARGE]: 64 * 1024,
-    [GitlabRunnerPreset.AMD_64_2XLARGE]: 128 * 1024,
+    [GitlabRunnerPreset.SMALL]: 8 * 1024,
+    [GitlabRunnerPreset.MEDIUM]: 16 * 1024,
+    [GitlabRunnerPreset.LARGE]: 32 * 1024,
+    [GitlabRunnerPreset.XLARGE]: 64 * 1024,
+    [GitlabRunnerPreset.TWO_XLARGE]: 128 * 1024,
 };
 
 export const GitlabRunnerCPUsPresetValue: Record<string, number> = {
-    [GitlabRunnerPreset.AMD_64_SMALL]: 2,
-    [GitlabRunnerPreset.AMD_64_MEDIUM]: 4,
-    [GitlabRunnerPreset.AMD_64_LARGE]: 8,
-    [GitlabRunnerPreset.AMD_64_XLARGE]: 16,
-    [GitlabRunnerPreset.AMD_64_2XLARGE]: 32,
+    [GitlabRunnerPreset.SMALL]: 2,
+    [GitlabRunnerPreset.MEDIUM]: 4,
+    [GitlabRunnerPreset.LARGE]: 8,
+    [GitlabRunnerPreset.XLARGE]: 16,
+    [GitlabRunnerPreset.TWO_XLARGE]: 32,
 };
