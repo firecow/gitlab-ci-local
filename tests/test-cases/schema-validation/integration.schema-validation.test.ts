2493c9dfdae16308994ff38ccaf6dfae17012520
ANGkeith <angkeith@hotmail.sg>
2024-07-24 14:36:43 +0800

    refactor: use asserts instead of process.exit (#1290)

--- tests/test-cases/schema-validation/integration.schema-validation.test.ts
+++ tests/test-cases/schema-validation/integration.schema-validation.test.ts
@@ -2,6 +2,9 @@ import {WriteStreamsMock} from "../../../src/write-streams";
 import {handler} from "../../../src/handler";
 import {initSpawnSpy} from "../../mocks/utils.mock";
 import {WhenStatics} from "../../mocks/when-statics";
+import assert, {AssertionError} from "assert";
+import {stripAnsi} from "../../utils";
+
 
 beforeAll(() => {
     initSpawnSpy(WhenStatics.all);
@@ -9,21 +12,19 @@ beforeAll(() => {
 
 test("schema validation <test-job>", async () => {
     const writeStreams = new WriteStreamsMock();
-    const mockExit = jest.spyOn(process, "exit").mockImplementation(() => {
-        // Do nothing
-        return undefined as never;
-    });
-
-    await handler({
-        cwd: "tests/test-cases/schema-validation",
-    }, writeStreams);
-    expect(writeStreams.stderrLines.join("\n")).toContain("Invalid .gitlab-ci.yml configuration!");
-    expect(writeStreams.stderrLines.join("\n")).toContain("property 'script' must not have fewer than 1 characters");
-    expect(writeStreams.stderrLines.join("\n")).toContain("'when' property must be one of [on_success, on_failure, always, never, manual, delayed] (found manual2)");
-    expect(writeStreams.stderrLines.join("\n")).toContain("'junit' property type must be string");
-    expect(writeStreams.stderrLines.join("\n")).toContain("'data' property is not expected to be here");
-
-    expect(mockExit).toHaveBeenCalledWith(1);
+    try {
+        await handler({
+            cwd: "tests/test-cases/schema-validation",
+        }, writeStreams);
+        expect(true).toBe(false);
+    } catch (e: any) {
+        assert(e instanceof AssertionError, "e is not instanceof AssertionError");
+        expect(e.message).toContain("Invalid .gitlab-ci.yml configuration!");
+        expect(e.message).toContain("property 'script' must not have fewer than 1 characters");
+        expect(e.message).toContain("'when' property must be one of [on_success, on_failure, always, never, manual, delayed] (found manual2)");
+        expect(e.message).toContain("'junit' property type must be string");
+        expect(e.message).toContain("'data' property is not expected to be here");
+    }
 });
 
 test("schema validation - default <test-job>", async () => {
@@ -56,4 +57,74 @@ my-job:
 
     expect(writeStreams.stdoutLines[0]).toEqual(expected);
     expect(writeStreams.stderrLines.join("\n")).toContain("my-job.artifacts is null, ignoring.");
+});
+
+test("schema validation 4 errors", async () => {
+    const writeStreams = new WriteStreamsMock();
+    try {
+        await handler({
+            file: ".gitlab-ci-4-errors.yml",
+            cwd: "tests/test-cases/schema-validation",
+
+        }, writeStreams);
+        expect(true).toBe(false);
+    } catch (e: any) {
+        const expected = `
+Invalid .gitlab-ci.yml configuration!
+\t• 'variables' property type must be object at error1.variables
+\t• 'variables' property type must be object at error2.variables
+\t• 'variables' property type must be object at error3.variables
+\t• 'variables' property type must be object at error4.variables
+
+`;
+        assert(e instanceof AssertionError, "e is not instanceof AssertionError");
+        expect(stripAnsi(e.message)).toContain(expected);
+    }
+});
+
+test("schema validation 5 errors", async () => {
+    const writeStreams = new WriteStreamsMock();
+    try {
+        await handler({
+            file: ".gitlab-ci-5-errors.yml",
+            cwd: "tests/test-cases/schema-validation",
+
+        }, writeStreams);
+        expect(true).toBe(false);
+    } catch (e: any) {
+        const expected = `Invalid .gitlab-ci.yml configuration!
+\t• 'variables' property type must be object at error1.variables
+\t• 'variables' property type must be object at error2.variables
+\t• 'variables' property type must be object at error3.variables
+\t• 'variables' property type must be object at error4.variables
+\t• 'variables' property type must be object at error5.variables
+
+For further troubleshooting, consider either of the following:`;
+        assert(e instanceof AssertionError, "e is not instanceof AssertionError");
+        expect(stripAnsi(e.message)).toContain(expected);
+    }
+});
+
+test("schema validation 6 errors", async () => {
+    const writeStreams = new WriteStreamsMock();
+    try {
+        await handler({
+            file: ".gitlab-ci-6-errors.yml",
+            cwd: "tests/test-cases/schema-validation",
+
+        }, writeStreams);
+        expect(true).toBe(false);
+    } catch (e: any) {
+        const expected = `Invalid .gitlab-ci.yml configuration!
+\t• 'variables' property type must be object at error1.variables
+\t• 'variables' property type must be object at error2.variables
+\t• 'variables' property type must be object at error3.variables
+\t• 'variables' property type must be object at error4.variables
+\t• 'variables' property type must be object at error5.variables
+\t... and 1 more
+
+`;
+        assert(e instanceof AssertionError, "e is not instanceof AssertionError");
+        expect(stripAnsi(e.message)).toContain(expected);
+    }
 });
