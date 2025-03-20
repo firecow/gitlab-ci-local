0574e6d3cbf10b4f5e7283ab26042299354fed90
ANGkeith <angkeith@hotmail.sg>
2025-03-20 21:53:13 +0800

    fix: Boolean false value on inputs breaks configuration syntax (#1529)

--- src/parser.ts
+++ src/parser.ts
@@ -409,7 +409,8 @@ function parseIncludeInputs (ctx: any): {inputValue: any; inputType: InputType} 
 
 function getInputValue (ctx: any) {
     const {inputs, interpolationKey, configFilePath, inputsSpecification} = ctx;
-    const inputValue = inputs[interpolationKey] || inputsSpecification.spec.inputs[interpolationKey]?.default;
+    const inputValue = inputs[interpolationKey] ??
+        inputsSpecification.spec.inputs[interpolationKey]?.default;
     assert(inputValue !== undefined, chalk`This GitLab CI configuration is invalid: \`{blueBright ${configFilePath}}\`: \`{blueBright ${interpolationKey}}\` input: required value has not been provided.`);
     return inputValue;
 }
