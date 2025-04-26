efac182dbae468ca0003ebfa87e0ecaa7b91ad79
Anton Kaiukov <batkowm@gmail.com>
2025-04-26 09:09:17 -0700

    fix: Fix inputs parsing when ctx.inputs is undefined. (#1555)
    * Fix inputs parsing when ctx.inputs is undefined.
    
    * Remove unused import in basic-inputs/integration.test.ts

---
+++ tests/test-cases/basic-inputs/integration.test.ts
@@ -0,0 +1,31 @@
+import {WriteStreamsMock} from "../../../src/write-streams.js";
+import {handler} from "../../../src/handler.js";
+import {initSpawnSpy} from "../../mocks/utils.mock.js";
+import {WhenStatics} from "../../mocks/when-statics.js";
+
+beforeAll(() => {
+    initSpawnSpy(WhenStatics.all);
+});
+
+test("basic-inputs defaults no inputs", async () => {
+    const writeStreams = new WriteStreamsMock();
+    await handler({
+        cwd: "tests/test-cases/basic-inputs/input-templates/default-no-inputs",
+        preview: true,
+    }, writeStreams);
+
+    const expected = `---
+stages:
+  - .pre
+  - test
+  - .post
+scan-website:
+  script:
+    - echo string
+    - echo 1
+    - echo true
+    - alice
+    - bob`;
+
+    expect(writeStreams.stdoutLines[0]).toEqual(expected);
+});
