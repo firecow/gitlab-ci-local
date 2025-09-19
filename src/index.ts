1a616c77d15a38808a97145c2707bff4973e786f
Mads Jon Nielsen <madsjon@gmail.com>
2025-09-19 13:49:18 +0200

    Release 4.62.0

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ process.on("SIGUSR2", async () => await cleanupJobResources(jobs));
     const yparser = yargs(process.argv.slice(2));
     yparser.parserConfiguration({"greedy-arrays": false})
         .showHelpOnFail(false)
-        .version("4.61.1")
+        .version("4.62.0")
         .wrap(yparser.terminalWidth?.())
         .command({
             handler: async (argv) => {
