2493fc986c2fb9a601bbd66a25903de8304b186e
Mads Jon Nielsen <madsjon@gmail.com>
2025-12-20 19:21:52 +0100

    Major upgrade jest (#1717)
    * Update jest
    
    * Remove package
    
    * Raise test a little
    
    * More timeout

--- tests/test-cases/services/integration.test.ts
+++ tests/test-cases/services/integration.test.ts
@@ -5,7 +5,7 @@ import {initSpawnSpy} from "../../mocks/utils.mock.js";
 import {WhenStatics} from "../../mocks/when-statics.js";
 import fs from "fs-extra";
 
-import.meta.jest.setTimeout(30000);
+import.meta.jest.setTimeout(60000);
 
 beforeAll(() => {
     initSpawnSpy(WhenStatics.all);
