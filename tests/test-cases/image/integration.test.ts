64ed7dca3031fd512647e6770d19fa00e1b7eddc
Mads Jon Nielsen <madsjon@gmail.com>
2026-02-14 19:32:03 +0100

    Restore test.concurrent for image and umask tests

--- tests/test-cases/image/integration.test.ts
+++ tests/test-cases/image/integration.test.ts
@@ -10,7 +10,7 @@ beforeAll(() => {
     initSpawnSpy(WhenStatics.all);
 });
 
-test("image <test job>", async () => {
+test.concurrent("image <test job>", async () => {
     const writeStreams = new WriteStreamsMock();
     await handler({
         cwd: "tests/test-cases/image",
@@ -23,7 +23,7 @@ test("image <test job>", async () => {
     expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
 });
 
-test("image <test-entrypoint>", async () => {
+test.concurrent("image <test-entrypoint>", async () => {
     const writeStreams = new WriteStreamsMock();
     await handler({
         cwd: "tests/test-cases/image",
@@ -41,7 +41,7 @@ test("image <test-entrypoint>", async () => {
     expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
 });
 
-test("image <test-entrypoint-override>", async () => {
+test.concurrent("image <test-entrypoint-override>", async () => {
     const writeStreams = new WriteStreamsMock();
     await handler({
         cwd: "tests/test-cases/image",
@@ -54,7 +54,7 @@ test("image <test-entrypoint-override>", async () => {
     expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
 });
 
-test("image <test-from-scratch>", async () => {
+test.concurrent("image <test-from-scratch>", async () => {
     const writeStreams = new WriteStreamsMock();
     await handler({
         cwd: "tests/test-cases/image",
@@ -74,7 +74,7 @@ test("image <test-from-scratch>", async () => {
 // ignored. The bug would cause this to also ignore test-file.txt in ./folder,
 // which it should not. Expected output will differ if ./folder/test-file.txt
 // is also ignored.
-test("image <test-ignore-regression>", async () => {
+test.concurrent("image <test-ignore-regression>", async () => {
     const writeStreams = new WriteStreamsMock();
 
     try {
@@ -93,7 +93,7 @@ test("image <test-ignore-regression>", async () => {
     expect(writeStreams.stdoutLines).toEqual(expect.arrayContaining(expected));
 });
 
-test("image <issue-206>", async () => {
+test.concurrent("image <issue-206>", async () => {
     const writeStreams = new WriteStreamsMock();
 
     await handler({
@@ -107,7 +107,7 @@ test("image <issue-206>", async () => {
     expect(writeStreams.stderrLines).toEqual(expect.arrayContaining(expected));
 });
 
-test("image <image-user>", async () => {
+test.concurrent("image <image-user>", async () => {
     const writeStreams = new WriteStreamsMock();
 
     await handler({
