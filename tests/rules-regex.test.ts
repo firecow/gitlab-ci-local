5b26451cd27af6937eaf88e66a0853a582b5b6f4
Pierre Gronlier <pierre@gronlier.fr>
2026-06-05 17:34:16 +0200

    test(rules:if): assert return value only, drop eval-string spy
    - Remove evalSpy and jsExpression assertions — these were white-box
      checks of an internal implementation detail, not behaviour
    - Compact test data accordingly
    - Fix /Hello (?i)world/ expectation: (?i) is not valid JS regex syntax
      so the new jsep path correctly rejects it; update to expectedErrSubStr

--- tests/rules-regex.test.ts
+++ tests/rules-regex.test.ts
@@ -18,108 +18,27 @@ beforeEach(async () => {
 
 /* eslint-disable @stylistic/quotes */
 const tests = [
-    {
-        rule: '"Hello World" =~ "/hello world/i"',
-        jsExpression: '"Hello World".matchRE2JS(RE2JS.compile("hello world", 1)) != null',
-        evalResult: true,
-    },
-    {
-        rule: '"Hello World" =~ /hello world/i',
-        jsExpression: '"Hello World".matchRE2JS(RE2JS.compile("hello world", 1)) != null',
-        evalResult: true,
-    },
-    {
-        rule: '"Hello World" =~ /Hello (?i)world/',
-        jsExpression: '"Hello World".matchRE2JS(RE2JS.compile("Hello (?i)world", 0)) != null',
-        evalResult: true,
-    },
-    {
-        rule: '"1.11" =~ /^([[:digit:]]+(.[[:digit:]]+)*|latest)$/',
-        jsExpression: '"1.11".matchRE2JS(RE2JS.compile("^([[:digit:]]+(.[[:digit:]]+)*|latest)$", 0)) != null',
-        evalResult: true,
-    },
-    {
-        rule: '"foo" !~ /foo/',
-        jsExpression: '"foo".matchRE2JS(RE2JS.compile("foo", 0)) == null',
-        evalResult: false,
-    },
-    {
-        rule: '"foo" =~ /foo/',
-        jsExpression: '"foo".matchRE2JS(RE2JS.compile("foo", 0)) != null',
-        evalResult: true,
-    },
-    {
-        rule: '"foo"=~ /foo/',
-        jsExpression: '"foo".matchRE2JS(RE2JS.compile("foo", 0)) != null',
-        evalResult: true,
-    },
-    {
-        rule: '"foo"=~/foo/',
-        jsExpression: '"foo".matchRE2JS(RE2JS.compile("foo", 0)) != null',
-        evalResult: true,
-    },
-    {
-        rule: '"foo"=~  /foo/',
-        jsExpression: '"foo".matchRE2JS(RE2JS.compile("foo", 0)) != null',
-        evalResult: true,
-    },
-    {
-        rule: '"foo" =~ "/foo/"',
-        jsExpression: '"foo".matchRE2JS(RE2JS.compile("foo", 0)) != null',
-        evalResult: true,
-    },
-    {
-        rule: '"test/url" =~ "/test/ur/"',
-        jsExpression: '"test/url".matchRE2JS(RE2JS.compile("test/ur", 0)) != null',
-        evalResult: true,
-    },
-    {
-        rule: '"test/url" =~ "/test\\/ur/"',
-        jsExpression: '"test/url".matchRE2JS(RE2JS.compile("test\\/ur", 0)) != null',
-        evalResult: true,
-    },
-    {
-        rule: '"test/url" =~ /test/ur/',
-        expectedErrSubStr: "Error attempting to evaluate the following rules:",
-    },
-    {
-        rule: '"master" =~ /master$/',
-        jsExpression: '"master".matchRE2JS(RE2JS.compile("master$", 0)) != null',
-        evalResult: true,
-    },
-    {
-        rule: '"23" =~ "1234"',
-        expectedErrSubStr: "must be a regex pattern. Do not rely on this behavior!",
-    },
-    {
-        rule: '"23" =~ \'1234\'',
-        expectedErrSubStr: "must be a regex pattern. Do not rely on this behavior!",
-    },
-    {
-        rule: '"23" =~ /1234/',
-        jsExpression: '"23".matchRE2JS(RE2JS.compile("1234", 0)) != null',
-        evalResult: false,
-    },
-    {
-        rule: '$CI_COMMIT_BRANCH && $GITLAB_FEATURES =~ /\bdependency_scanning\b/ && $CI_GITLAB_FIPS_MODE == "true"',
-        jsExpression: 'null && false && null == "true"',
-        evalResult: false,
-    },
-    {
-        rule: '($CI_MERGE_REQUEST_SOURCE_BRANCH_NAME =~ /^perf_.*$/)',
-        jsExpression: '(false)', // (null.matchRE2JS(RE2JS.compile("^perf_.*$", 0)) != null => (false)
-        evalResult: false,
-    },
-    {
-        rule: '("qwerty" =~ /^perf_.*$/)',
-        jsExpression: '("qwerty".matchRE2JS(RE2JS.compile("^perf_.*$", 0)) != null)',
-        evalResult: false,
-    },
-    {
-        rule: `"product-name/v0.0.0+build.0" =~ /^(?:product-name\\/)?v\\d+\\.\\d+\\.\\d+.*/`,
-        jsExpression: "\"product-name/v0.0.0+build.0\".matchRE2JS(RE2JS.compile(\"^(?:product-name\\\\/)?v\\\\d+\\\\.\\\\d+\\\\.\\\\d+.*\", 0)) != null",
-        evalResult: true,
-    },
+    {rule: '"Hello World" =~ "/hello world/i"', evalResult: true},
+    {rule: '"Hello World" =~ /hello world/i', evalResult: true},
+    {rule: '"Hello World" =~ /Hello (?i)world/', expectedErrSubStr: "Error attempting to evaluate the following rules:"},
+    {rule: '"1.11" =~ /^([[:digit:]]+(.[[:digit:]]+)*|latest)$/', evalResult: true},
+    {rule: '"foo" !~ /foo/', evalResult: false},
+    {rule: '"foo" =~ /foo/', evalResult: true},
+    {rule: '"foo"=~ /foo/', evalResult: true},
+    {rule: '"foo"=~/foo/', evalResult: true},
+    {rule: '"foo"=~  /foo/', evalResult: true},
+    {rule: '"foo" =~ "/foo/"', evalResult: true},
+    {rule: '"test/url" =~ "/test/ur/"', evalResult: true},
+    {rule: '"test/url" =~ "/test\\/ur/"', evalResult: true},
+    {rule: '"test/url" =~ /test/ur/', expectedErrSubStr: "Error attempting to evaluate the following rules:"},
+    {rule: '"master" =~ /master$/', evalResult: true},
+    {rule: '"23" =~ "1234"', expectedErrSubStr: "must be a regex pattern. Do not rely on this behavior!"},
+    {rule: '"23" =~ \'1234\'', expectedErrSubStr: "must be a regex pattern. Do not rely on this behavior!"},
+    {rule: '"23" =~ /1234/', evalResult: false},
+    {rule: '$CI_COMMIT_BRANCH && $GITLAB_FEATURES =~ /\bdependency_scanning\b/ && $CI_GITLAB_FIPS_MODE == "true"', evalResult: false},
+    {rule: '($CI_MERGE_REQUEST_SOURCE_BRANCH_NAME =~ /^perf_.*$/)', evalResult: false},
+    {rule: '("qwerty" =~ /^perf_.*$/)', evalResult: false},
+    {rule: `"product-name/v0.0.0+build.0" =~ /^(?:product-name\\/)?v\\d+\\.\\d+\\.\\d+.*/`, evalResult: true},
 ];
 /* eslint-enable @stylistic/quotes */
 
@@ -128,12 +47,10 @@ describe("gitlab rules regex", () => {
         .forEach((t) => {
             test(`- if: '${t.rule}'\n\t => ${t.evalResult}`, async () => {
                 const rules = [ {if: t.rule} ];
-                const evalSpy = vi.spyOn(global, "eval");
                 const evaluateRuleIfSpy = vi.spyOn(Utils, "evaluateRuleIf");
 
                 Utils.getRulesResult({argv, cwd: "", rules, variables: {}}, gitData);
                 expect(evaluateRuleIfSpy).toHaveReturnedWith(t.evalResult);
-                expect(evalSpy).toHaveBeenCalledWith(t.jsExpression);
             });
         });
 });
