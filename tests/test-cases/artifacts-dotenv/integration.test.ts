11ae6998eea6a5ba82844876899b31f4011e4ac6
Alexander Fedotov <anprowh@gmail.com>
2026-08-27 11:58:54 +0300

    test: add failed job dotenv report pass test
    reports are always uploaded regardless of job results:
    https://docs.gitlab.com/ci/yaml/artifacts_reports/

--- tests/test-cases/artifacts-dotenv/integration.test.ts
+++ tests/test-cases/artifacts-dotenv/integration.test.ts
@@ -13,9 +13,12 @@ test.concurrent("artifacts-dotenv <use-image-ref> --needs", async () => {
         cwd: "tests/test-cases/artifacts-dotenv",
         job: ["use-image-ref"],
         needs: true,
+        noColor: true,
         stateDir: ".gitlab-ci-local-artifacts-dotenv-use-image-ref-needs",
     }, writeStreams);
 
+    // The job only runs at all if BUILD_IMAGE_REF expanded into `image`
+    expect(writeStreams.stdoutLines.join("\n")).toMatch(/use-image-ref\s+> NAME="Alpine Linux"/);
     expect(writeStreams.stderrLines.join("\n")).not.toMatch(/FAIL/);
 });
 
@@ -25,8 +28,27 @@ test.concurrent("artifacts-dotenv <use-service-ref> --needs", async () => {
         cwd: "tests/test-cases/artifacts-dotenv",
         job: ["use-service-ref"],
         needs: true,
+        noColor: true,
         stateDir: ".gitlab-ci-local-artifacts-dotenv-use-service-ref-needs",
     }, writeStreams);
 
+    // SERVICE_IMAGE_ALIAS expanded into the service alias, so it resolves in DNS
+    expect(writeStreams.stdoutLines.join("\n")).toMatch(/use-service-ref\s+> redis has address/);
     expect(writeStreams.stderrLines.join("\n")).not.toMatch(/FAIL/);
+});
+
+test.concurrent("artifacts-dotenv <use-non-success-ref> --needs", async () => {
+    const writeStreams = new WriteStreamsMock();
+    await handler({
+        cwd: "tests/test-cases/artifacts-dotenv",
+        job: ["use-non-success-ref"],
+        needs: true,
+        noColor: true,
+        stateDir: ".gitlab-ci-local-artifacts-dotenv-use-non-success-ref-needs",
+    }, writeStreams);
+
+    const stdout = writeStreams.stdoutLines.join("\n");
+    expect(stdout).toMatch(/use-non-success-ref\s+> FAILURE_REF is \[failure-value\]/);
+    expect(stdout).toMatch(/use-non-success-ref\s+> WARNING_REF is \[warning-value\]/);
+    expect(writeStreams.stderrLines.join("\n")).not.toMatch(/use-non-success-ref.*FAIL/);
 });
